46bc9573f764921cd6ea198d7e21dba550de1b6a
dianagatcan <gatcan.diana@gmail.com>
2022-03-23 16:23:10 +0200

    👌 Working version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,67 +4,118 @@ import Button from "./Button/Button";
 import * as math from 'mathjs';
 
 
+//handle pressing equal twice
+
+
+
 export default function App(){
 
-    const keys=[7,8,9, '*', 4, 5, 6, '/', 1, 2, 3, '+', 'C', 0, '=',  '-']
+    const keys=[7,8,9, '*', 4, 5, 6, '/', 1, 2, 3, '+', '.', 0, '=', '-']
 
-    const [value, setValue] = useState('')
+    const [value, setValue] = useState('0')
     const [operator, setOperator] = useState('')
-    const [secondValue, setSecondValue] = useState('')
+    const [secondValue, setSecondValue] = useState('0')
+    const [result,setResult] = useState("")
+
+    function isOperator(){
+        return operator ? true : false;
+    }
+
+    function handleEqual(){
+        setResult(math.evaluate(`${value}${operator}${secondValue}`).toString())
+        setValue("0")
+        setSecondValue("0")
+        setOperator("")
+    }
 
 
+//--------------------------------------------------------------------------------------------------------------------------------------------    
 
-    // function displayText(buttonValue){
-    //     if(buttonValue === "="){
-    //         setValue(math.evaluate(value))
-    //     } else{
-    //         setValue(prevValue => {return prevValue + buttonValue})
-    //     }
-    // }
 
+    function isOperatorPressed(string){
+        return ['+','-','*','/','='].some((value) => {
+            return value===string
+        })
+    }
+
+    function isOrangePressed(string){
+        return ['+','-','*','/'].some((value) => {
+            return value===string
+        })
+    }
+
+    function changeState(number){
+            if(!isOperator()){
+                if(value.includes('.') && number ==='.'){return}
+                setValue((prevValue) =>{return trimFirstZero(prevValue+number)})
+            }
+            else{
+                if(secondValue.includes('.') && number ==='.'){return}
+                setSecondValue((prevValue) =>{return trimFirstZero(prevValue+number)})
+            }
+        
+    }
 
     
     function displayText(buttonValue){
-
-        if(operator !=="" && secondValue==="" && buttonValue === 0 || buttonValue === '.'){
-            setSecondValue("")
-        } else if(operator !=="" && typeof buttonValue === 'number' || buttonValue === '.'){
-            setSecondValue(prevValue => {return prevValue + buttonValue})
-        } else if(value === "" && buttonValue === 0 || buttonValue === '.'){
-            setValue("")
-        }else if(typeof buttonValue === 'number' || buttonValue === '.'){
-            setValue(prevValue => {return prevValue + buttonValue})
+        if(isOperatorPressed(buttonValue)){
+            if(result!==""){
+                setValue(result)
+                setResult("")
+            }
+            if(isOrangePressed(buttonValue)){
+                setOperator(buttonValue)
+            }
+            if(buttonValue === '='){
+                handleEqual()
+            }
         }
-        if(buttonValue === "+" || buttonValue === "-" || buttonValue === "*" || buttonValue === "/"){
-            setOperator(buttonValue)
+        else{
+            if(result!==""){setResult("")}
+            changeState(buttonValue)
         }
-        if(secondValue !=="" && buttonValue === "+" || buttonValue === "-" || buttonValue === "*" || buttonValue === "/" ){
-            setValue(math.evaluate(`${value}${operator}${secondValue}`))
-            setSecondValue("")
-        }
-        if(buttonValue === "=" && value !== "" && secondValue !== ""){
-            setValue(math.evaluate(`${value}${operator}${secondValue}`))
-            setSecondValue("")
-            setOperator("")
-        }
-
+            
     }
 
+
+    function decideDisplay(){
+        if(result!==""){
+            return result
+        }
+        else{
+            return secondValue !=="0" ? secondValue : value
+        }
+
+        
+    }
 
   
 
     function clearState(){
-        setValue("")
+        setValue("0")
         setOperator("")
-        setSecondValue("")
+        setSecondValue("0")
+        setResult("")
     }
 
     return (<div className="main">
-    <Screen value={secondValue !=="" ? secondValue : value} />
+    <Screen value={decideDisplay()} />
     <div className="wrapper">
-     {(keys.map((buttonValue, index) => <Button key={index} id={index} text={buttonValue} handleClick={() => {displayText(buttonValue)}} clear={clearState} />))}
-
+     {(keys.map((buttonValue, index) => <Button key={index} id={index} text={buttonValue} handleClick={() => {displayText(buttonValue)}} />))}
+     <button className="clear" onClick={clearState}>Clear</button>
     </div>
     
     </div>)
+}
+
+/**
+ * 
+ * @param {string} string 
+ * @returns 
+ */
+function trimFirstZero(string){
+    if(string[0]==='0' && !string.includes('.')){
+        return string.substring(1)
+    }
+    return string
 }
