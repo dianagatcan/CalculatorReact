33c401135f2241388e6a6b1153c012c4d116c2d2
dianagatcan <gatcan.diana@gmail.com>
2022-03-23 16:13:28 +0200

    👌 Handle point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,9 @@ import Button from "./Button/Button";
 import * as math from 'mathjs';
 
 
-/**
- * 
- * 
- * 
- * 
- */
+//handle pressing equal twice
+
+
 
 export default function App(){
 
@@ -24,27 +21,6 @@ export default function App(){
         return operator ? true : false;
     }
 
-    function displayValue(buttonValue){
-            //we want to display first value only when no operator is selected
-        if(!isOperator() && (typeof buttonValue === "number" || buttonValue==='.')){
-            setValue(prevValue => {return trimFirstZero(prevValue + buttonValue)})
-        }
-    }
-
-    function handleOperator(buttonValue){
-        setOperator(buttonValue)
-        if(isOperator()){
-            handleEqual();
-        }
-    }
-    
-
-    function displaySecondValue(buttonValue){
-        if(isOperator() && (typeof buttonValue === "number" || buttonValue==='.' )){
-            setSecondValue(prevValue => {return trimFirstZero(prevValue + buttonValue)})
-        }
-    }
-
     function handleEqual(){
             setResult(math.evaluate(`${value}${operator}${secondValue}`).toString())
             setValue(0)
@@ -55,8 +31,9 @@ export default function App(){
 
 //--------------------------------------------------------------------------------------------------------------------------------------------    
 
+
     function isOperatorPressed(string){
-        return ['+','-','*','/','.','='].some((value) => {
+        return ['+','-','*','/','='].some((value) => {
             return value===string
         })
     }
@@ -68,12 +45,15 @@ export default function App(){
     }
 
     function changeState(number){
-        if(!isOperator()){
-            setValue((prevValue) =>{return trimFirstZero(prevValue+number)})
-        }
-        else{
-            setSecondValue((prevValue) =>{return trimFirstZero(prevValue+number)})
-        }
+            if(!isOperator()){
+                if(value.includes('.') && number ==='.'){return}
+                setValue((prevValue) =>{return trimFirstZero(prevValue+number)})
+            }
+            else{
+                if(secondValue.includes('.') && number ==='.'){return}
+                setSecondValue((prevValue) =>{return trimFirstZero(prevValue+number)})
+            }
+        
     }
 
     
@@ -94,6 +74,7 @@ export default function App(){
             if(result!==""){setResult("")}
             changeState(buttonValue)
         }
+            
     }
 
 
@@ -114,6 +95,7 @@ export default function App(){
         setValue("0")
         setOperator("")
         setSecondValue("0")
+        setResult("")
     }
 
     return (<div className="main">
